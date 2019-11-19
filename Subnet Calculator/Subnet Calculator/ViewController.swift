e6bbed034fbacbc235ed680c93f8937077132a95
Richard Stockdale <richardstockdale@mac.com>
2019-11-19 21:38:09 +0000

    Initial Commit

---
+++ Subnet Calculator/Subnet Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Subnet Calculator
+//
+//  Created by Richard Stockdale on 19/11/2019.
+//  Copyright © 2019 RGB Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
